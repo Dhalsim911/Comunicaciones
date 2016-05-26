e2a6d22a2141b96b2203b57ccfed9cc6acfaf9c3
Esteban Vargas <estebanva911@gmail.com>
2016-05-26 12:42:51 -0600

    Parte 3 a medio palo, falta graficar

--- Taller3/Taller3.m
+++ Taller3/Taller3.m
@@ -1,5 +1,7 @@
+
 %o1 = sqrt(2/Tsim)*cos(2*pi*fc*t);
 %02 = -sqrt(2/Tsim)*cos(2*pi*fc*t);
+tb = 0.003;  
 
 %%% Parte 2: Secuencias de bit de entrada %%%
 imagen = imread('Super_Mario.jpg');
@@ -7,3 +9,9 @@ Ximg = im2bw(imagen,0.5);
 %imshow(imagen);
 imshow(Ximg);
 ximg = reshape(Ximg, [1,307200]);
+
+%%%Parte 3: Modulación 16-QAM banda-base %%%
+M = 16 %Simbolos poosibles
+b = 4 %bits por simbolo
+Tsim = b*tb;
+
