dedb55c7ad8b8ba853fa5cdc427d4cf839baaea9
Berni Mora <bgme171@hotmail.com>
2016-06-05 13:20:10 -0600

    demodulador 16qam

--- Taller3/Taller3.m
+++ Taller3/Taller3.m
@@ -1,17 +1,20 @@
 
-%o1 = sqrt(2/Tsim)*cos(2*pi*fc*t);
-%02 = -sqrt(2/Tsim)*cos(2*pi*fc*t);
-tb = 0.003;  
+Tb = 0.003;
+Tsim = 4*Tb;
+%f1 = sqrt(2/Tsim)*cos(2*pi*fc*t);
+%f2 = -sqrt(2/Tsim)*cos(2*pi*fc*t);
+
 
 %%% Parte 2: Secuencias de bit de entrada %%%
-imagen = imread('Super_Mario.jpg');
-Ximg = im2bw(imagen,0.5);
+imagen = imread('Charmander.png');
+Ximg = im2bw(imagen, 0.5);
 %imshow(imagen);
 imshow(Ximg);
 ximg = reshape(Ximg, [1,307200]);
 
 %%%Parte 3: Modulación 16-QAM banda-base %%%
-M = 16 %Simbolos poosibles
-b = 4 %bits por simbolo
-Tsim = b*tb;
-
+%M = 16 %Simbolos poosibles
+%b = 4 %bits por simbolo
+%Tsim = b*tb;
+h = modem.qammod('M', 16, 'SymbolOrder', 'Gray', 'InputType', 'Bit')
+y = modulate(Ximg,10,100,'qam')
