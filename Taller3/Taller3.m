e48472d64b5b7676305721c8138ab81c452167d6
Berni Mora <bgme171@hotmail.com>
2016-06-05 17:21:27 -0600

    Parte 4 y 5 avanzadas

--- Taller3/Taller3.m
+++ Taller3/Taller3.m
@@ -10,11 +10,47 @@ imagen = imread('Charmander.png');
 Ximg = im2bw(imagen, 0.5);
 %imshow(imagen);
 imshow(Ximg);
-ximg = reshape(Ximg, [1,307200]);
+ximgvector = reshape(Ximg, [1,307200]);
 
 %%%Parte 3: Modulación 16-QAM banda-base %%%
-%M = 16 %Simbolos poosibles
-%b = 4 %bits por simbolo
-%Tsim = b*tb;
-h = modem.qammod('M', 16, 'SymbolOrder', 'Gray', 'InputType', 'Bit')
-y = modulate(Ximg,10,100,'qam')
+ximg = reshape(Ximg,307200/4,4); 
+xsimb = bi2de(ximg);   
+%h = modem.qammod('M', 16, 'SymbolOrder', 'Gray', 'InputType', 'Bit')
+%y = modulate(Ximg,10,100,'qam')
+figure; % Create new figure window.
+stem(xsimb(1:250));
+title('Primeros simbolos transmitidos');
+xlabel('Symbol Index');
+ylabel('Integer Value');
+
+dataMod = qammod(xsimb,16,0,'gray');
+scatterplot(dataMod,1,0,'k*')
+
+%%%Parte 4: Canal AWGN banda-base %%%
+snr = 25; %SNR en dB
+rsimb = awgn(dataMod,snr,'measured');
+
+%figure; % Create new figure window.
+%stem(rsimb(1:250));
+%title('Primeros simbolos recibidos');
+%xlabel('Symbol Index');
+%ylabel('Integer Value');
+
+sPlotFig = scatterplot(rsimb,1,0,'g.');
+hold on
+scatterplot(dataMod,1,0,'k*',sPlotFig)
+
+%%%Parte 5: Demodulación 16-QAM banda-base %%%
+ysimb = qamdemod(rsimb,16,0,'gray');
+yimg = dec2bin(ysimb,4);
+yimgvector = reshape(yimg, [1,307200]);
+
+[numErrors,ber] = biterr(ximgvector,yimgvector);
+fprintf('\nThe binary coding bit error rate = %5.2e, based on %d errors\n', ber,numErrors);
+
+[m,n] = size(Ximg)
+Yimg = reshape(yimg,m,n);
+%A = logical(Yimg);
+%image(Yimg);
+
+
